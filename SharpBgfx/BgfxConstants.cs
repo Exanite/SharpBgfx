53fee45f98ebab54d828598935633d5b5e639443
Exanite <exanite78012@gmail.com>
2023-12-02 15:31:43 -0500

    Add constants for invalid handles

--- SharpBgfx/BgfxConstants.cs
+++ SharpBgfx/BgfxConstants.cs
@@ -1,6 +1,21 @@
+using SharpBgfx.Bindings;
+
 namespace SharpBgfx;
 
 public class BgfxConstants
 {
-    public const ushort InvalidHandle = ushort.MaxValue;
+    public const ushort InvalidHandleId = ushort.MaxValue;
+
+    public static readonly bgfx.DynamicIndexBufferHandle InvalidDynamicIndexBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.DynamicVertexBufferHandle InvalidDynamicVertexBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.FrameBufferHandle InvalidFrameBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.IndexBufferHandle InvalidIndexBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.IndirectBufferHandle InvalidIndirectBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.OcclusionQueryHandle InvalidOcclusionQueryHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.ProgramHandle InvalidProgramHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.ShaderHandle InvalidShaderHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.TextureHandle InvalidTextureHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.UniformHandle InvalidUniformHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.VertexBufferHandle InvalidVertexBufferHandle = new() { idx = InvalidHandleId };
+    public static readonly bgfx.VertexLayoutHandle InvalidVertexLayoutHandle = new() { idx = InvalidHandleId };
 }
